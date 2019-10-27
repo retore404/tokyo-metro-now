612bbb1024e9e5b9dbed2ff16e1a77ac075a3e5c
retore404 <pvt5417@gmail.com>
2019-10-27 16:07:24 +0900

    update:表示領域のサンプル作成

--- common/akasaka_mitsuke_now/views.py
+++ common/akasaka_mitsuke_now/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'akasaka_mitsuke_now/index.html', {
-        'val': 'test',
+        'deptTime': '19:14',
+        'destination': '荻窪',
+        'note': '荻窪方面',
+        'getTime': '19:10'
     })
