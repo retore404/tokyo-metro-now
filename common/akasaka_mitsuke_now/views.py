d666d5fa2eaea8ab069c1a6cdf92c0a131401351
retore404 <pvt5417@gmail.com>
2019-11-01 06:08:02 +0900

    update:データ取得とA線データの抜き出し

--- common/akasaka_mitsuke_now/views.py
+++ common/akasaka_mitsuke_now/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 from django.conf import settings
 
+import requests
+
 def index(request):
-    print(getattr(settings, "API_KEY", None))
+    # settings.pyに設定したAPIキーの読み込み
+    apiKey = getattr(settings, "API_KEY", None)
+    
+    # 丸ノ内線走行中全列車の走行位置情報を取得
+    trainUrlRoot = 'https://api.tokyometroapp.jp/api/v2/datapoints'
+    
+    response = requests.get(
+        trainUrlRoot,
+        params = {'rdf:type': 'odpt:Train',
+                  'acl:consumerKey': apiKey,
+                  'odpt:railway': 'odpt.Railway:TokyoMetro.Marunouchi'}
+    )
+    
+    # レスポンスから荻窪方面・方南町方面（A線）の列車を抜き出し
+    data = response.json()
+    data = [train for train in data if train['odpt:railDirection'] == 'odpt.RailDirection:TokyoMetro.Ogikubo' or train['odpt:railDirection'] == 'odpt.RailDirection:TokyoMetro.Honancho']
+    
+
+    for t in data:
+        print(t)
+
     return render(request, 'akasaka_mitsuke_now/index.html', {
         'deptTime': '19:14',
         'destination': '荻窪',
